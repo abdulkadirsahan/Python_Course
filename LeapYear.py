f456133e8781f5d99165b90fb1e75836d83b8bed
abdulkadirsahan1 <sahan1965@hotmail.com>
2023-06-15 12:10:46 +0300

    Pizza projesi güncellendi  LeapYear e not eklendi

--- LeapYear.py
+++ LeapYear.py
@@ -3,13 +3,13 @@ year = int(input("Which year do you want to check? "))
 
 if year%4==0:
     if year%100==0:
-        if year&100==0:
+        if year&400==0:
             print("Leap year.")
         else:
             print("Not leap year.")       # if'i iç içe yazarken 2.if i eklediğin de 1.satıra elseyi ekle ve 2.if den devam et.
     else:
-        print("Leap year.")
-else:
+        print("Leap year.")                 #İç içe if'lerde pc ilk olarak büyük resme bakar.Sonra if deyiminin içindeki iç içe geçmiş                                           #bloğa
+else:                                       #bloğa mı girmesi gerektiğine karar verir.
     print("Not leap year.")
 
 
--- Project_pizza.py
+++ Project_pizza.py
@@ -1,15 +1,49 @@
-
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
-size1=15
-size2=20
-size3=25
+size = input("What size pizza do you want? S, M, or L : ")
+add_pepperoni = input("Do you want pepperoni? Y or N :")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+small=15
+cheese=1
+medium=20
+large=25
 
 if size=="S":
-    
-    print("Ödemeniz gereken miktar 15$")
-
+    if add_pepperoni=="Y":
+        smallpepperoni=2
+        if extra_cheese=="Y":
+              total=small+cheese+smallpepperoni
+              print(f"Total price:${total}")
+        else:
+            total=smallpepperoni+small
+            print(f"Total price=${total}")
+    else:
+        total=small
+        print(f"Total price ${total}")
 if size=="M":
-     print("Ödemeniz gereken miktar $20")
+    if add_pepperoni=="Y":
+        mediumpepperoni=3
+        if extra_cheese=="Y":
+            total=medium+mediumpepperoni+cheese
+            print(f"Total price ${total}")
+        else:
+            total=medium+mediumpepperoni
+            print(f"Total price ${total}")
+    else:
+        total=medium
+        print(f"Total price:${total}")
+if size=="L":
+    if add_pepperoni=="Y":
+        largepepperoni=3
+        if extra_cheese=="Y":
+            total=large+largepepperoni+cheese
+            print(f"Total price ${total}")
+        else:
+            total=large+largepepperoni
+            print(f"Total price ${total}")
+    else:
+        total=large
+        print(f"Total price:${total}")
+
+
+
