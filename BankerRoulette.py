a86f8752f386a0ac11325b47a9db06fa5610906d
abdulkadirsahan1 <sahan1965@hotmail.com>
2023-06-17 12:45:18 +0300

    Listeler eklendi

--- BankerRoulette.py
+++ BankerRoulette.py
@@ -3,9 +3,11 @@ import random
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 
-tp=len(names)
-rndm=random.randint(0,tp-1)
-rndmprsn=names[rndm]
+#tp=len(names)
+#rndm=random.randint(0,tp-1)
+#rndmprsn=names[rndm]
 
-print(rndmprsn + " You will pay")
+#print(rndmprsn + " You will pay")
 
+randm=random.choice(names)
+print(f" {randm} you will pay")
